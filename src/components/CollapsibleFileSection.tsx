234dc0a22dc423286593f1ba8f6a3105dfaf9db6
ZHOUWEI <zhouwei.will@gmail.com>
2026-05-15 15:57:10 +0800

    style: 统一所有"在访达中打开"图标为 macOS Finder 风格
    CollapsibleFileSection 和 UninstallAppsList 中的 external-link SVG 替换为 Finder 图标，与 SelectionSummary 保持一致

--- src/components/CollapsibleFileSection.tsx
+++ src/components/CollapsibleFileSection.tsx
@@ -63,10 +63,8 @@ function FileRow({ file, showCheckbox, checkedFiles, onToggleFile, iconSrc }: {
         {iconSrc ? (
           <img src={iconSrc} alt="Finder" className="w-3.5 h-3.5" />
         ) : (
-          <svg xmlns="http://www.w3.org/2000/svg" className="w-3.5 h-3.5 text-macos-text-tertiary" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-            <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6" />
-            <polyline points="15 3 21 3 21 9" />
-            <line x1="10" y1="14" x2="21" y2="3" />
+          <svg className="w-3.5 h-3.5" viewBox="0 0 1024 1024" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M180.544 196.992l389.44 0c0 0 63.488 1.984 64.64 57.728 1.152 56.384-64.64 56.512-64.64 56.512L360.576 311.232c0 0-49.6-7.552-69.44 44.48C283.456 375.872 217.664 559.488 192.064 640c-7.232 22.72 10.048 28.864 24.128 29.248 14.72 0.512 27.968 0.256 40.192 0 16.384-0.128 30.272-6.4 37.888-29.248 16.384-49.728 69.696-182.4 76.608-199.872C388.736 395.008 412.16 399.616 438.016 399.616c47.168 0 491.712 0 491.712 0 61.12 0 84.032 33.024 66.304 83.968L921.6 849.344C902.592 908.032 845.44 960 784.32 960L182.656 960C118.912 960 64 905.6 64 844.48l0-534.4C64 249.024 119.488 196.992 180.544 196.992z" fill="currentColor" />
           </svg>
         )}
       </button>
